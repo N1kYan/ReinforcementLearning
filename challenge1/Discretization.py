1022bdf075c0741b69a3c9ef6bbcaf58c5a24d4b
NikYan <yannik_phil.frisch@stud.tu-darmstadt.de>
2018-11-22 22:53:08 +0100

    new file for Discretization class; now doing dynamic programming on discrete space; still some troubles

---
+++ challenge1/Discretization.py
@@ -0,0 +1,33 @@
+# TODO: Doc
+# This deiscretization can currentyl be used for the 
+# ai gym pendulum environment
+
+import numpy as np
+
+class Discretization:
+    
+    def __init__(self, name, platform):
+        self.name = name
+        self.platform = platform
+        # Determine action space 
+        # and sice of state space used for look-up-tables for value function and policy
+        if(self.platform=="Pendulum"):
+            self.action_space_size = 17
+            self.action_space = np.linspace(-2,2,self.action_space_size)
+            self.state_space_size = (21, 21, 33)
+            self.state_space = (np.linspace(-1, 1, self.state_space_size[0]),
+                                np.linspace(-1, 1, self.state_space_size[1]),
+                                np.linspace(-8, 8, self.state_space_size[2]))
+
+    # Maps discrete state to index for value function or policy lookup table
+    def map_to_index(self, x):
+        if(self.platform=="Pendulum"):
+            index = [int(10*(x[0]+1)), int(10*(x[1]+1)), int(2*(x[2]+8))]
+            return index
+    
+    # Maps index back to state
+    def map_to_state(self, x):
+        if(self.platform=="Pendulum"):
+            state = [(x[0]/10)-1, (x[1]/10)-1, (x[2]/2)-8]
+            return state
+       
