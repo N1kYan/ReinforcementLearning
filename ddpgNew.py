5206f4035263a194ea549f0eb423d18e57cadd8d
TabeaW <sonstiges.tabea@t-online.de>
2018-11-25 16:32:59 +0100

    forgotten some keywords

--- ddpgNew.py
+++ ddpgNew.py
@@ -171,7 +171,7 @@ class ActorCriticTargets(object):
         K.set_session(self.tensor)
 
         self.actor = Actor(state_space, action_space, learningrate, hidden_neurons, self.session)
-        self.actor_input_dim, self.actor_model, self.actor_target_model = actor.create_network()
+        self.actor_input_dim, self.actor_model, self.actor_target_model = self.actor.create_network()
         self.actor_weights = self.actor_model.trainable_weights
         self.merge_grad = tf.placeholder(tf.float32, [None, action_space]) # this will be the saving place for the deterministic policy gradient (theorem)
         self.actor_gradient = tf.gradients(self.actor_model.output, self.actor_weights, -self.merge_grad)
@@ -179,7 +179,7 @@ class ActorCriticTargets(object):
         self.optimizing = tf.train.AdamOptimizer(learningrate).apply_gradients(zip(self.actor_gradient, self.actor_weights))
 
         self.critic = Critic(state_space, action_space, learningrate, hidden_neurons, self.session)
-        self.state_input, self.action_input, self.critic_model, self.critic_target_model = critic.create_network()
+        self.state_input, self.action_input, self.critic_model, self.critic_target_model = self.critic.create_network()
         self.critic_gradient = tf.gradients(self.critic_model.output, self.action_input)
 
         self.tensor.run(tf.initializ_all_variables())
