adfef0aec081ff6a726faa338e42033f50dd7aa7
TabeaW <sonstiges.tabea@t-online.de>
2018-11-25 16:30:49 +0100

    added training methods, they won't work!

--- ddpgNew.py
+++ ddpgNew.py
@@ -2,12 +2,11 @@ import tensorflow as tf
 import numpy as np
 import gym
 import quanser_robots
-import torch.nn as nn
-import torch.nn.functional as F
-import torch
-from keras.layers import Dense, Input, Activation
+import tensorflow as tf
+from keras.layers import Dense, Input, Activation, Add, Multiply
 from keras.optimizers import Adam
 from keras.models import Model
+import keras.backend as K
 
 import random
 from collections import deque
@@ -35,7 +34,7 @@ class Replay(object):
         times = list(zip(*expectations))[4]
         return states, actions, rewards, next_states, times
 
-
+'''
 class Actor(nn.Module):
     def __init__(self, state_size, action_size, learningrate, batch_size, hidden_neurons, action_limit):
         self.learning = learningrate
@@ -90,3 +89,108 @@ class Critic(nn.Module):
 
         out = self.out(out)
         return out
+'''
+
+
+class Actor(object):
+    def __init__(self, state_size, action_size, learningrate, hidden_neurons, action_dim, session):
+        self.learning = learningrate
+        self.state_space = state_size
+        self.action_space = action_size
+        self.hidden = hidden_neurons
+        self.action_dim = action_dim
+        self.input = None #will hold the input layer
+        self.output = None #will hold the output layer
+        self.nn = None #will hold the model of the nn
+        self.tensor_session = session
+
+    def create_network(self):
+        self.input = Input(self.state_space)
+        h1 = Dense(self.hidden, activation='relu')(self.input)
+        output = Dense(self.action_space, activation='tanh')(h1)
+        self.output = Multiply(self.action_dim)(output)
+        self.nn = Model(input=self.input, output=self.output)
+        adam_optimizer = Adam(self.learning)
+        self.nn.compile(loss='mse', optimizer=adam_optimizer)
+        return self.input, self.nn, self.nn
+
+    def train(self, optimizer, input, action_gradient, merge):
+        merged_grad = merge
+        self.tensor_session.run(optimizer, feed_dict={self.input: input, merged_grad:action_gradient})
+
+    def predict(self, input):
+        self.tensor_session.run(self.output, feed_dict={self.input:input})
+
+    def update_target(self, actor, critic):
+        return
+
+class Critic(object):
+    def __init__(self, state_size, action_size, learningrate, hidden_neurons, session):
+        self.learning = learningrate
+        self.state_space = state_size
+        self.action_space = action_size
+        self.hidden = hidden_neurons
+        self.tensor_session = session
+        self.state_input = None
+        self.action_input = None
+        self.output = None
+        self.nn = None
+
+    def create_network(self):
+        self.state_input = Input(self.state_space)
+        self.action_input = Input(self.action_space)
+        s1 = Dense(self.hidden, activation='relu')(self.state_input)
+        a1 = Dense(self.hidden, activation='relu')(self.action_input)
+
+        merge = Add()(self.state_input, self.action_input)
+        m1 = Dense(self.hidden, activation='relu')(merge)
+
+        self.output = Dense(1, activation='relu')(m1)
+
+        self.nn = Model(input= [self.state_input, self.action_input], output=self.output)
+        adam_optimizer = Adam(self.learning)
+        self.nn.compile(loss='mse', optimizer=adam_optimizer)
+        return self.state_input, self.action_input, self.nn, self.nn
+
+    def train(self, optimizer, merge, state, action, pred_q):
+        self.tensor_session.run([self.output, optimizer], feed_dict={self.state_input:state, self.action_input:action, merge:pred_q})
+
+    def predict(self, state, action):
+        self.tensor_session.run(self.output, feed_dict={self.state_input: state, self.action_input:action})
+
+
+class ActorCriticTargets(object):
+    def __init__(self, state_space, action_space, learningrate, hidden_neurons):
+        self.replay = Replay(5000)
+        self.tau = 0.1
+        self.gamma = 0.9
+        self.epsilon = 1.0
+        self.eps_dec = 0.9
+
+        self.tensor = tf.Session()
+        K.set_session(self.tensor)
+
+        self.actor = Actor(state_space, action_space, learningrate, hidden_neurons, self.session)
+        self.actor_input_dim, self.actor_model, self.actor_target_model = actor.create_network()
+        self.actor_weights = self.actor_model.trainable_weights
+        self.merge_grad = tf.placeholder(tf.float32, [None, action_space]) # this will be the saving place for the deterministic policy gradient (theorem)
+        self.actor_gradient = tf.gradients(self.actor_model.output, self.actor_weights, -self.merge_grad)
+
+        self.optimizing = tf.train.AdamOptimizer(learningrate).apply_gradients(zip(self.actor_gradient, self.actor_weights))
+
+        self.critic = Critic(state_space, action_space, learningrate, hidden_neurons, self.session)
+        self.state_input, self.action_input, self.critic_model, self.critic_target_model = critic.create_network()
+        self.critic_gradient = tf.gradients(self.critic_model.output, self.action_input)
+
+        self.tensor.run(tf.initializ_all_variables())
+
+    def train(self):
+        batch_size = 64
+        if self.replay.size<batch_size:
+            return
+        sample = self.replay.random_batch(self.replay, batch_size)
+        self.critic.train(self.critic_model)
+        self.actor.train(self.actor_model)
+
+env = gym.make('Qube-v0')
+print(env.action_space.shape[0])
