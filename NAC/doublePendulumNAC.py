00c177b66ab7e34a349a36ff160f12ac3366ac3d
NikYan <yannik_phil.frisch@stud.tu-darmstadt.de>
2018-11-30 13:13:39 +0100

    nac file

--- NAC/doublePendulumNAC.py
+++ NAC/doublePendulumNAC.py
@@ -3,6 +3,17 @@ import numpy as np
 from quanser_robots import GentlyTerminating
 from quanser_robots.double_pendulum.examples import metronom
 env = GentlyTerminating(gym.make('DoublePendulum-v0'))
+"""
+    The DoublePendulum-v0 environment:
+    
+    The state space is 6 dimensional:
+     (x, theta1, theta2, x dot, theta1 dot, theta2 dot)
+    Min: (-2, -pi, -30, -40) Max: (2, pi, 30, 40)
+    
+    The action space is 1 dimensional.
+    Min -15 Max: 15
+"""
+
 ctrl = ...  # some function f: s -> a
 obs = env.reset()
 
@@ -22,8 +33,8 @@ def evaluate(env, crtl=None):
     env.close()
 
 def main():
-    ctrl = metronom.MetronomCtrl()
-    evaluate(env, ctrl)
+    #ctrl = metronom.MetronomCtrl()
+    evaluate(env)
 
 if __name__ == "__main__":
     main()
