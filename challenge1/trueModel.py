f292cbc6f67e0e8adf7f6ad513287faac7ef0f54
Maximilian Alexander Gehrke <max.alex@online.de>
2018-12-02 15:03:14 +0100

    Add TestingRewards

--- challenge1/trueModel.py
+++ challenge1/trueModel.py
@@ -12,6 +12,13 @@ from quanser_robots.pendulum.pendulum_v2 import angle_normalize
 
 
 def transition(input):
+    """
+    Return the next state, given an action and the current state.
+    :param input: Array consisting of three elements. [0] is the current
+    states angle in degree, [1] is the current states velocity and [2] is
+    the action we want to take.
+    :return: The state after we take the given action
+    """
     state = np.array([input[0], input[1]])
     action = input[2]
     g = 10.0
@@ -19,18 +26,28 @@ def transition(input):
     m = 1.0
     dt = 0.05
     action = np.clip(action, -2.0, 2.0)
-    newthdot = state[1] + (-3 * g / (2 * l) * np.sin(state[0] + np.pi) + 3. / (m * l ** 2) * action) * dt
+    newthdot = state[1] + (-3 * g / (2 * l) * np.sin(state[0] + np.pi) + 3. /
+                           (m * l ** 2) * action) * dt
     newth = state[0] + newthdot * dt
     newthdot = np.clip(newthdot, -8, 8)
     return np.array([newth, newthdot])
 
 
 def reward(input):
+    """
+    Calculate the reward of an action, given a current state.
+    :param input: Array consisting of three elements. [0] is the current
+    states angle in degree, [1] is the current states velocity and [2] is
+    the action we want to take.
+    :return: The reward for the action from a given state.
+    """
     state = np.array([input[0], input[1]])
     action = input[2]
     action = np.clip(action, -2.0, 2.0)
     costs = - angle_normalize(state[0])**2 + .1 * state[1]**2 + .001 * (action**2)
+
     return costs
+
 
 def main():
     env = gym.make("Pendulum-v2")
@@ -48,6 +65,7 @@ def main():
     print("True state: ", s)
     print("True reward: ",r)
     print()
+
 
 if __name__ == "__main__":
     main()
