deb653163eefc04f2bf5202f3abb169785f1266c
Maximilian Alexander Gehrke <max.alex@online.de>
2018-12-03 14:16:27 +0100

    Utils  visualize funktion nun mit state_space

--- challenge1/Utils.py
+++ challenge1/Utils.py
@@ -10,17 +10,17 @@ def print_array(arr):
         print(i)
 
 
-def visualize(value_function, policy, disc=None):
+def visualize(value_function, policy, state_space=None):
     plt.figure()
     plt.title("Value function")
     plt.imshow(value_function)
     plt.colorbar()
 
-    if disc is not None:
+    if state_space is not None:
         plt.ylabel("Angle in Radians")
-        plt.yticks(range(disc.state_space_size[0]), labels=disc.state_space[0].round(2))
+        plt.yticks(range(len(state_space[0])), labels=state_space[0].round(2))
         plt.xlabel("Velocity")
-        plt.xticks(range(disc.state_space_size[1]), labels=disc.state_space[1].round(1))
+        plt.xticks(range(len(state_space[1])), labels=state_space[1].round(1))
 
     plt.show()
 
@@ -28,10 +28,10 @@ def visualize(value_function, policy, disc=None):
     plt.imshow(policy)
     plt.colorbar()
 
-    if disc is not None:
+    if state_space is not None:
         plt.ylabel("Angle in Radians")
-        plt.yticks(range(disc.state_space_size[0]), labels=disc.state_space[0].round(2))
+        plt.yticks(range(len(state_space[0])), labels=state_space[0].round(2))
         plt.xlabel("Velocity")
-        plt.xticks(range(disc.state_space_size[1]), labels=disc.state_space[1].round(1))
+        plt.xticks(range(len(state_space[1])), labels=state_space[1].round(1))
 
     plt.show()
