3534fc3fdc5dd8cb748a805e67c8e3830fdf45c1
NikYan <yannik_phil.frisch@stud.tu-darmstadt.de>
2018-12-15 12:19:23 +0100

    Gaussian state transition probabilites

--- challenge1/valueIteration.py
+++ challenge1/valueIteration.py
@@ -4,6 +4,7 @@ import gym
 import quanser_robots
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal
 from Regression import Regressor
 from Utils import *
 from Evaluation import *
@@ -34,9 +35,12 @@ def value_iteration(S, A, P, R, gamma, theta):
     # Initialize value function with bad values
     V = np.zeros(shape=np.shape(S)[0]*[np.shape(S)[1]-1])
     V.fill(-20)
+    goal = get_index(S, [0, 0])
+    V[goal[0]][goal[1]] = 0
     # Fill policy with neutral action (mid of action space)
     PI = np.zeros(shape=np.shape(S)[0]*[np.shape(S)[1]-1])
     PI.fill(A[0][int(len(A[0][:])/2)])
+    print(A[0][int(len(A[0][:])/2)])
 
     t = 1
     states = np.stack(np.meshgrid(range(len(S[0][:])-1),range(len(S[1][:])-1))).T.reshape(-1, 2)
@@ -83,13 +87,11 @@ def value_iteration(S, A, P, R, gamma, theta):
     return V, PI
 
 
-def evaluate_discrete_space(S, A):
-    print(np.shape(S)[1])
+def evaluate_discrete_space(S, A, gaussian_sigmas):
 
     P = np.zeros(shape=(np.shape(S)[0] * [np.shape(S)[1]-1] +
                         np.shape(A)[0] * [np.shape(A)[1]-1] +
                         np.shape(S)[0] * [np.shape(S)[1]-1]))
-    print(P.shape)
     # R = np.zeros(shape=(S_shape[0] * [S_shape[1]] + A_shape[0] * [A_shape[1]]))
 
     # This reward 'function' is only defined for the successor states
@@ -110,7 +112,7 @@ def evaluate_discrete_space(S, A):
     # TODO: more efficent way?
     print("Evaluating state transition function ... ", end='')
     sys.stdout.flush()
-    states_action = np.stack(np.meshgrid(range(P.shape[0]),range(P.shape[1]), range(P.shape[2]))).T.reshape(-1,3)
+    states_action = np.stack(np.meshgrid(range(P.shape[0]),range(P.shape[1]), range(P.shape[2]))).T.reshape(-1, 3)
     for s0, s1, a in states_action:
         # Successor of state (s0, s1) for action a
         # We use [0] because we only have one state
@@ -120,9 +122,18 @@ def evaluate_discrete_space(S, A):
         ns0 = ns[0]
         ns1 = ns[1]
 
-        main_p = 1.0
+        # main_p = 1.0
+        # P[s0][s1][a][ns0][ns1] = main_p
 
-        P[s0][s1][a][ns0][ns1] = main_p
+        successor_indices = np.stack(np.meshgrid(range(P.shape[3]), range(P.shape[4]))).T.reshape(-1, 2)
+
+        cov = np.eye(2, 2)
+        cov[0][0] = gaussian_sigmas[0]
+        cov[1][1] = gaussian_sigmas[1]
+
+        for i0, i1 in successor_indices:
+            P[s0][s1][a][i0][i1] = multivariate_normal.pdf(x=np.array([i0, i1]), mean=np.array([ns0, ns1]), cov=cov)
+
 
         """
         P[s0][s1][a][np.clip(ns0 - 1, a_min=0, a_max=S_shape[1] - 2)][
@@ -160,7 +171,7 @@ def build_discrete_space():
     observation_size = len(state)
     observation_range = (env.observation_space.low, env.observation_space.high)
     # TODO: Different bins for different dimensions
-    observation_bins = observation_size*[20]
+    observation_bins = observation_size*[40]
     S = []
     for i in range(observation_size):
         S.append(np.linspace(observation_range[0][i], observation_range[1][i], observation_bins[i]))
@@ -244,9 +255,13 @@ def main(make_plots):
 
     reg.perform_regression(env=env, epochs=10000, save_flag=False)
     S, A = build_discrete_space()
-    P, R = evaluate_discrete_space(S=S, A=A)
-    V, PI = value_iteration(S=S, A=A, P=P, R=R, gamma=0.8, theta=1e-2)
-    state_distribution = evaluate(S=S, episodes=10, policy=PI, render=False, sleep=0, epsilon_greedy=0.01)
+    P, R = evaluate_discrete_space(S=S, A=A, gaussian_sigmas=np.array([10, 10]))
+
+    plt.figure()
+    plt.imshow(P[1][1][1][:][:])
+
+    V, PI = value_iteration(S=S, A=A, P=P, R=R, gamma=0.8, theta=1e-4)
+    state_distribution = evaluate(S=S, episodes=10, policy=PI, render=True, sleep=0, epsilon_greedy=0.01)
 
     if make_plots:
         visualize(value_function=V, policy=PI, R=R, state_distribution=state_distribution, state_space=S)
