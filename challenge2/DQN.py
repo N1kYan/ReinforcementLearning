dbe6044c1d8d0fb3423cfad4e45f4c143e4c1481
TabeaW <sonstiges.tabea@t-online.de>
2018-12-28 10:44:34 +0100

    DQN running, but not working perfect

--- challenge2/DQN.py
+++ challenge2/DQN.py
@@ -1,21 +1,17 @@
 import gym
 import quanser_robots
 import numpy as np
-import matplotlib.pyplot as plt
 from MemoryDQN import MemoryDQN
 from DQNNet import DQN
 import math
 import random
 import matplotlib.pyplot as plt
-from collections import namedtuple
-from itertools import count
+
 
 import torch
-import torch.nn as nn
 from torch.autograd import Variable
 import torch.optim as optim
 import torch.nn.functional as F
-import torchvision.transforms as T
 
 
 class ActionDisc(gym.Space):
@@ -44,10 +40,10 @@ LongTensor = torch.LongTensor
 
 EPISODES = 200
 BATCH_SIZE = 64
-GAMMA = 0.8
+GAMMA = 0.9
 HIDDEN_LAYER_NEURONS = 100
 LEARNING_RATE = 0.001
-ACTION_SPACE = 49
+ACTION_SPACE = 49 #49
 
 EPS_END = 0.05
 EPS_START = 0.9
@@ -65,7 +61,7 @@ state = env.reset()
 step = 0
 steps_done = 0
 action = np.ndarray([1,])
-
+cum_reward = []
 def select_action(state_pred):
     global steps_done
     sample = random.random()
@@ -91,50 +87,57 @@ def select_action(state_pred):
 
 
 for epi in range(EPISODES):
-
+    cum_reward.append(0)
+    state = env.reset()
+    step = 0
+    steps_done = 0
+    print(epi)
     while True:
-        act = select_action(state)
-        #action[0]=act[0].numpy()
-        action = act
-        #act = action.numpy()
-        print(action.numpy())
-        state_follows, reward, done, info = env.step(act.numpy()[0])
-        #state_follows = np.concatenate(state_follows)
-        #memory.add_observation(FloatTensor([state]), action, FloatTensor([reward]), FloatTensor([state_follows]))
+        action = select_action(state)
+        state_follows, reward, done, info = env.step(action.numpy()[0])
+
+        cum_reward[epi] += reward
+
         memory.add_observation(state, action, reward, state_follows)
-        env.render()
+
         if epi == EPISODES - 1:
             env.render()
         if done:
-            reward=-1
+            reward = -1
         #training
-        if memory.size_mem()> BATCH_SIZE:
+        if memory.size_mem() > BATCH_SIZE:
             states, actions, rewards, next_states = memory.random_batch(BATCH_SIZE)
-            # current Q values are estimated by NN for all actions
 
             states = torch.from_numpy(states).type(FloatTensor)
+
             next_states = torch.from_numpy(next_states).type(FloatTensor)
 
-            #actions = torch.from_numpy(actions).type(FloatTensor).unsqueeze(0)
+            #find the index to the given action
             actions = env.action_space.contains(actions)
+            actions = np.array(actions).repeat(ACTION_SPACE).reshape(BATCH_SIZE,ACTION_SPACE)
             actions = LongTensor(actions)
-            #print(np.shape(states), np.shape(actions))
-            current_q_values = model(states).gather(dim=1, index=actions)
+
+            #for each q-value(for each state in the batch and for each action), take the one from the chosen action
+            current_q_values = model(states).gather(dim=1, index=actions)[:, 0]
+
             # expected Q values are estimated from actions which gives maximum Q value
             max_next_q_values = model(next_states).detach().max(1)[0]
             expected_q_values = rewards + (GAMMA * max_next_q_values)
-            print(expected_q_values)
-            # loss is measured from error between current and newly expected Q values
-            loss = F.smooth_l1_loss(current_q_values, expected_q_values)
 
-            # backpropagation of loss to NN
+            loss = F.smooth_l1_loss(current_q_values, expected_q_values.type(FloatTensor))
+            #loss = F.mse_loss(current_q_values, expected_q_values.type(FloatTensor))
+
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-
         state = state_follows
         step += 1
 
-        if done:
+        '''if done:
+            break'''
+        if step == 500:
             break
+
+plt.plot(cum_reward)
+plt.show()
--- challenge2/MemoryDQN.py
+++ challenge2/MemoryDQN.py
@@ -24,7 +24,8 @@ class MemoryDQN(object):
         actions = list(zip(*expectations))[1]
         rewards = list(zip(*expectations))[2]
         next_states = list(zip(*expectations))[3]
-        return np.array(states), np.array(actions), np.array(rewards), np.array(next_states)
+        return torch.from_numpy(np.array(states)).type(torch.FloatTensor), np.array(actions), np.array(rewards),\
+               torch.from_numpy(np.array(next_states)).type(torch.FloatTensor)
         #return Variable(torch.cat(states)), Variable(torch.cat(actions)), Variable(torch.cat(rewards)), Variable(torch.cat(next_states))
 
     def size_mem(self):
