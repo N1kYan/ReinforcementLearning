0b5deafb6688935e9b7c4382689e3b4c2cc41a4f
N1kYan <yannik_phil.frisch@stud.tu-darmstadt.de>
2019-02-20 20:22:52 +0100

    update for REINFORCE

--- challenge3/REINFORCE.py
+++ challenge3/REINFORCE.py
@@ -9,17 +9,17 @@ from keras import backend as K
 
 
 class ReinforceAgent:
-    def __init__(self, state_space_size, action_space_size, learning_rate, discounting):
+    def __init__(self, state_space_size, action_space, learning_rate, discounting, load_weights):
         self.state_space_size = state_space_size
-        self.action_space_size = action_space_size
-        self.render_flag = False
-        self.load_weights_flag = False
+        self.action_space = action_space
+        self.load_weights_flag = load_weights
         self.gamma = discounting
         self.learning_rate = learning_rate
         self.model = self.init_model()
         self.model.summary()
         self.learning_rate = learning_rate
-        self.states, self.gradients, self.rewards, self.probs = [], [], [], []
+        self.trajectory_states, self.trajectory_actions, self.trajectory_probs, self.trajectory_rewards \
+            = [], [], [], []
         if self.load_weights_flag:
             self.model.load_weights("./saved_models/REINFORCE")
 
@@ -29,38 +29,47 @@ class ReinforceAgent:
         :return: Keras model
         """
         model = Sequential()
-        model.add(Dense(20, input_dim=self.state_space_size, activation='elu'))
-        model.add(Dense(20, activation='elu'))
-        model.add(Dense(self.action_space_size, activation='softmax'))
-        model.summary()
+        model.add(Dense(10, input_dim=self.state_space_size, activation='elu'))
+        model.add(Dense(10, activation='elu'))
+        model.add(Dense(self.action_space.size, activation='softmax'))
         model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=self.learning_rate))
         return model
 
     def remember(self, state, action, prob, reward):
-        y = np.zeros([self.action_space_size])
-        y[action] = 1
-        self.gradients.append(np.array(y).astype('float32') - prob)
-        self.states.append(state)
-        self.rewards.append(reward)
+        """
+        Saving a trajectory.
+        :param state: current state s of the environment
+        :param action: current performed action a
+        :param reward: current observed reward r
+        :return: none
+        """
+        self.trajectory_states.append(state)
+        self.trajectory_actions.append(action)
+        self.trajectory_probs.append(prob)
+        self.trajectory_rewards.append(reward)
 
     def predict_action(self, state):
         """
-        Forward pas through the nn policy network.
-        Approximating a ~ pi(a|s)
+        Forward pass through the nn policy network.
+        Approximating a ~ pi(a|s) and then random sampling from disc. action space.
         :param state: Current state s of the observation.
-        :return: Predicted action a
+        :return: Predicted action a, probability of this action
         """
         state = state.reshape([1, state.shape[0]])
         action_probs = self.model.predict(state, batch_size=1).flatten()
-        self.probs.append(action_probs)
-        probs = action_probs/np.sum(action_probs)
-        action = np.random.choice(self.action_space_size, 1, p=probs)[0]
-        return action, probs
+        action = np.random.choice(self.action_space, 1, p=action_probs)[0]
+        # return action, action_probs[np.where(self.action_space == action)]
+        return action, action_probs
 
     def discounted_rewards(self, rewards):
+        """
+        Calculate discounted rewards from trajectory.
+        :param rewards: Remembered rewards from trajectory (per time-step)
+        :return:
+        """
         discounted_rewards = np.zeros_like(rewards)
         running_add = 0
-        for t in reversed(range(0, rewards.size)):
+        for t in reversed(range(0, len(rewards))):
             if rewards[t] != 0:
                 running_add = 0
             running_add = running_add * self.gamma + rewards[t]
@@ -68,15 +77,23 @@ class ReinforceAgent:
         return discounted_rewards
 
     def train(self):
-        gradients = np.vstack(self.gradients)
-        rewards = self.discounted_rewards(np.vstack(self.rewards))
-        rewards = rewards / np.std(rewards - np.mean(rewards))
-        gradients *= rewards
+        """
+        Calculate loss and weight update.
+        Update parameters
+        :return:
+        """
+        # Iterate over trajectory
+        for t in range(0, len(self.trajectory_states)):
+            # Get log gradient
+            log_gradient = K.gradients(np.log(self.trajectory_probs[t]), self.model.trainable_weights)
+            print("Log gradient:", log_gradient)
+            # Update parameters
+            self.model.set_weights(self.model.trainable_weights + self.learning_rate
+                                   * log_gradient * self.discounted_rewards(self.trajectory_rewards[t:]))
 
-        X = np.squeeze(np.vstack([self.states]))
-        Y = self.probs + self.learning_rate * np.squeeze(np.vstack([gradients]))
-        self.model.train_on_batch(X, Y)
-        self.states, self.probs, self.gradients, self.rewards = [], [], [], []
+        # Reset memory
+        self.trajectory_states, self.trajectory_actions, self.trajectory_probs, self.trajectory_rewards =\
+            [], [], [], []
 
     def load_weights(self, name):
         self.model.load_weights(name)
@@ -85,30 +102,34 @@ class ReinforceAgent:
         self.model.save_weights(name)
 
 
-def training():
+def training(render=False):
     env = gym.make('Levitation-v1')
     print(env.spec.id)
     print("State Space:\t Shape: {}\t Low: {}\t High {}".format(env.observation_space.shape, env.observation_space.low,
                                                                 env.observation_space.high))
     print("Action Space:\t Shape: {}\t Low: {}\t High {}".format(env.action_space.shape, env.action_space.low,
                                                                  env.action_space.high))
-    print("Reward range:\t {}".format(env.reward_range))
-    agent = ReinforceAgent(state_space_size=env.observation_space.shape[0], action_space_size=env.action_space.shape[0],
-                           discounting=0.99, learning_rate=0.001)
+    print("Reward Range:\t {}".format(env.reward_range))
+    discrete_actions = np.linspace(start=env.action_space.low, stop=env.action_space.high, num=5)
+    print("Discrete Actions: ", discrete_actions)
+    agent = ReinforceAgent(state_space_size=env.observation_space.shape[0], action_space=discrete_actions,
+                           discounting=0.99, learning_rate=0.001, load_weights=False)
+
     epochs = 10
-    render = True
     for e in range(epochs):
         state = env.reset()
         cum_reward = [0]
         t = 0
         while True:
+            if render:
+                env.render()
             t += 1
             action, prob = agent.predict_action(state)
             action = np.array([action])
             # print("Action: ", action)
             state, reward, done, info = env.step(action)
             cum_reward.append(cum_reward[-1]+reward)
-            agent.remember(state, action, prob, reward)
+            agent.remember(state=state, action=action, prob=prob, reward=reward)
             if done:
                 agent.train()
                 print("Epoch {} done after {} time-steps. Cumulative reward: {}".format(
