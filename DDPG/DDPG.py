fa0f5fe555391ae8e00461ba2cc1f17b69279134
N1kYan <yannik_phil.frisch@stud.tu-darmstadt.de>
2019-03-15 06:06:28 +0100

    DDPG update

--- DDPG/DDPG.py
+++ DDPG/DDPG.py
@@ -344,11 +344,11 @@ def main():
 
     global env
     # env = gym.make('Qube-v0')
-    # env = gym.make('CartpoleSwingLong-v0')
+    env = gym.make('CartpoleStabShort-v0')
     # env = gym.make('Pendulum-v0')
     # env = GentlyTerminating(gym.make('BallBalancerRR-v0'))
     # env = gym.make('QubeRR-v0')
-    env = gym.make('BallBalancerSim-v0')
+    # env = gym.make('BallBalancerSim-v0')
     print(env.spec.id)
     print("State Space:\tShape:{}\tLow:{}\tHigh:{}".format(np.shape(env.reset()), env.observation_space.low,
                                                            env.observation_space.high))
@@ -367,16 +367,16 @@ def main():
     # Noise generating process
     OU_NOISE = OUNoise(size=env_specs[1], seed=random_seed, mu=0., theta=0.25, sigma=0.2)
 
-    GAUSS_NOISE = Gaussian(size=env_action_size, seed=random_seed, mu=0.0, sigma=2.8, decay=0.0)
+    GAUSS_NOISE = Gaussian(size=env_action_size, seed=random_seed, mu=0.0, sigma=3.5, decay=0.0)
 
     # Replay memory
-    MEMORY = ReplayBuffer(env=env, buffer_size=int(1e6), batch_size=64,
+    MEMORY = ReplayBuffer(env=env, buffer_size=int(1e6), batch_size=256,
                           seed=random_seed)
 
     # Run training procedure with defined hyperparameters
-    ACTOR = training(epochs=5000, max_steps=10000, epoch_checkpoint=500, noise=OU_NOISE, epsilon=None,
+    ACTOR = training(epochs=3000, max_steps=10000, epoch_checkpoint=500, noise=GAUSS_NOISE, epsilon=None,
                      epsilon_decrease=None, add_noise=True, lr_actor=1e-4, lr_critic=1e-3, weight_decay=0,
-                     gamma=0.99, memory=MEMORY, tau=1e-4, seed=random_seed, save_flag=True, load_flag=False,
+                     gamma=0.97, memory=MEMORY, tau=1e-3, seed=random_seed, save_flag=True, load_flag=False,
                      load_path='26-2-20', render=True, use_pretrained=False)
 
     # Run evaluation
