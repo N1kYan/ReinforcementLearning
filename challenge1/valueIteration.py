28ebedffe329308513b4d497cb1f80cda8acc4bd
N1kYan <yannik_phil.frisch@stud.tu-darmstadt.de>
2019-08-28 22:11:07 +0200

    small updates; fixing imports

--- challenge1/valueIteration.py
+++ challenge1/valueIteration.py
@@ -6,33 +6,32 @@ import quanser_robots
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
-from Regression import Regressor
-from Utils import *
-from Evaluation import *
+from challenge1.Regression import Regressor
+from challenge1.Utils import *
+from challenge1.Evaluation import *
 
 env = gym.make('Pendulum-v2')
 reg = Regressor()
 
 
 def value_iteration(S, A, P, R, gamma, theta):
-
     print("Value iteration... ")
 
     # Initialize value function with bad values
-    V = np.zeros(shape=np.shape(S)[0]*[np.shape(S)[1]-1])
+    V = np.zeros(shape=np.shape(S)[0] * [np.shape(S)[1] - 1])
     V.fill(-20)
     goal = get_observation_index(env=env, observation_s=S, x=[0, 0])
     V[goal[0]][goal[1]] = 0
     # Fill policy with neutral action (mid of action space)
-    PI = np.zeros(shape=np.shape(S)[0]*[np.shape(S)[1]-1])
-    PI.fill(A[0][int(len(A[0][:])/2)])
+    PI = np.zeros(shape=np.shape(S)[0] * [np.shape(S)[1] - 1])
+    PI.fill(A[0][int(len(A[0][:]) / 2)])
     t = 1
-    states = np.stack(np.meshgrid(range(len(S[0][:])-1),range(len(S[1][:])-1))).T.reshape(-1, 2)
+    states = np.stack(np.meshgrid(range(len(S[0][:]) - 1), range(len(S[1][:]) - 1))).T.reshape(-1, 2)
     while True:
         for s0, s1 in states:
             v = V[s0][s1]
-            Qsa = np.zeros(shape=np.shape(A)[0]*[np.shape(A)[1]-1])
-            for a in range(len(A[0][:])-1):
+            Qsa = np.zeros(shape=np.shape(A)[0] * [np.shape(A)[1] - 1])
+            for a in range(len(A[0][:]) - 1):
                 """
                 next_state = reg.regressorState.predict(np.array([S[0][s0], S[1][s1], A[0][a]]).reshape(1, -1))[0]
                 ns = get_index(env=env, space=S, x=next_state)
@@ -43,9 +42,9 @@ def value_iteration(S, A, P, R, gamma, theta):
                 Qsa[a] = sum(map(sum, (P[s0][s1][a][:][:] * (R[:][:] + gamma * V[:][:]))))
             max_Qsa = np.max(Qsa)
             V[s0][s1] = max_Qsa
-            delta = np.abs(v-max_Qsa)
+            delta = np.abs(v - max_Qsa)
         # Reduce discount factor per timestep
-        #gamma = gamma/t
+        # gamma = gamma/t
         t += 1
         print("Delta =", delta, end='')
         if delta < theta:
@@ -59,8 +58,8 @@ def value_iteration(S, A, P, R, gamma, theta):
     print("Defining Policy ...", end='')
     sys.stdout.flush()
     for s0, s1 in states:
-        Qsa = np.zeros(shape=np.shape(A)[0] * [np.shape(A)[1]-1])
-        for a in range(len(A[0][:])-1):
+        Qsa = np.zeros(shape=np.shape(A)[0] * [np.shape(A)[1] - 1])
+        for a in range(len(A[0][:]) - 1):
             """
             next_state = reg.regressorState.predict(np.array([S[0][s0], S[1][s1], A[0][a]]).reshape(1, -1))[0]
             ns = get_observation_index(env=env, observation_s=S, x=next_state)
@@ -75,6 +74,7 @@ def value_iteration(S, A, P, R, gamma, theta):
         PI[s0][s1] = max_action
     print("done")
     return V, PI
+
 
 """
 def evaluate_discrete_space(S, A, gaussian_sigmas):
@@ -135,25 +135,25 @@ def evaluate_discrete_space(S, A, gaussian_sigmas):
     return P, R
 """
 
-def main(make_plots):
 
+def main(make_plots):
     # Start time
     start = time.time()
 
-	# Define discrete spaces
+    # Define discrete spaces
     S, A = build_discrete_space(env=env)
-	# Sample
-	# Use save_flag=False to load from sample file
+    # Sample
+    # Use save_flag=False to load from sample file
     P, R = reg.sample(env=env, S=S, A=A, gaussian_sigmas=np.array([1, 1]), epochs=10000, save_flag=True)
-    #P, R = evaluate_discrete_space(S=S, A=A, gaussian_sigmas=np.array([1, 1]))
-	# Do value iteration
+    # P, R = evaluate_discrete_space(S=S, A=A, gaussian_sigmas=np.array([1, 1]))
+    # Do value iteration
     V, PI = value_iteration(S=S, A=A, P=P, R=R, gamma=0.75, theta=1e-15)
 
     # End time
     end = time.time()
-    print("\nTime elapsed: {} seconds \n".format(np.round(end-start, decimals=2)))
+    print("\nTime elapsed: {} seconds \n".format(np.round(end - start, decimals=2)))
 
-	# Evaluate policy from value iteration
+    # Evaluate policy from value iteration
     state_distribution = evaluate(env=env, S=S, episodes=10, policy=PI, render=True, sleep=0, epsilon_greedy=0.0)
 
     if make_plots:
